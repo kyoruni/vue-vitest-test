01dbfcae2f5eb856d4fe7e7a58b7acd8e26a740d
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-12-30 18:27:07 +0900

    :wrench: ユニットテスト 入力値が可変の場合

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -14,5 +14,18 @@ describe(
         expect(actual).toBe(expected);
       }
     );
+    test(
+      '左に10,右に9を入力した場合',
+      async () => {
+        const wrapper = mount(App);
+        // ユーザーの画面操作の代わりになるものは非同期で行われるようになっている
+        // 入力が終わってから次に進みたいので、awaitが必要
+        await wrapper.get('[data-test-id="num1"]').setValue(10);
+        await wrapper.get('[data-test-id="num2"]').setValue(9);
+        const actual = wrapper.get('[data-test-id="answer"]').text();
+        const expected = '19';
+        expect(actual).toBe(expected);
+      }
+    );
   }
 );
