2ad561d533566d4cc8f85771da70cea60b860a51
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-12-30 17:24:58 +0900

    :wrench: 割り算を追加

--- src/__tests__/functions.spec.ts
+++ src/__tests__/functions.spec.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest';
-import { addTwoNums } from '@/functions';
+import { addTwoNums, devideTwoNums } from '@/functions';
 
 describe(
   'functions.ts addTwoNums() test',
@@ -11,6 +11,42 @@ describe(
         const num2 = 2;
         const actual = addTwoNums(num1, num2);
         const expected = 3;
+        expect(actual).toBe(expected);
+      }
+    );
+  }
+);
+
+describe(
+  'functions.ts devideTwoNums() test',
+  () => {
+    test(
+      '1桁の割り算のテスト',
+      () => {
+        const num1 = 6;
+        const num2 = 2;
+        const actual = devideTwoNums(num1, num2);
+        const expected = 3;
+        expect(actual).toBe(expected);
+      }
+    );
+    test(
+      '分子が0のテスト',
+      () => {
+        const num1 = 0;
+        const num2 = 3;
+        const actual = devideTwoNums(num1, num2);
+        const expected = 0;
+        expect(actual).toBe(expected);
+      }
+    );
+    test(
+      '分母が0のテスト',
+      () => {
+        const num1 = 3;
+        const num2 = 0;
+        const actual = devideTwoNums(num1, num2);
+        const expected = Infinity;
         expect(actual).toBe(expected);
       }
     );
--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
 export function addTwoNums (num1: number, num2: number): number {
   return num1 + num2;
-}
+};
+
+export function devideTwoNums (num1: number, num2: number): number {
+  return num1 / num2;
+};
