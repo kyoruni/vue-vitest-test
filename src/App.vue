5486060b5d17181edb36275a69a2d1af121fe1a7
kyoruni <40832190+kyoruni@users.noreply.github.com>
2022-12-30 19:11:24 +0900

    :wrench: ユニットテスト ボタンを押した場合

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,34 @@
 import { ref, computed } from 'vue';
 import { addTwoNums } from '@/functions';
 
+// 足し算のテスト
 const num1 = ref(5);
 const num2 = ref(3);
 const answer = computed((): number => {
   return addTwoNums(Number(num1.value), Number(num2.value))
 });
+
+// 要素が存在するかどうかのテスト
+const isShow = ref(false);
+const onClickShowButton = () => {
+  isShow.value = !isShow.value
+};
 </script>
 
 <template>
   <div class="app">
-    <input type="nunber" data-test-id="num1" v-model="num1"/>
-    +
-    <input type="nunber" data-test-id="num2" v-model="num2"/>
-    =
-    <span data-test-id="answer">{{ answer }}</span>
+    <section>
+      <p>足し算のテスト</p>
+      <input type="nunber" data-test-id="num1" v-model="num1"/>
+      +
+      <input type="nunber" data-test-id="num2" v-model="num2"/>
+      =
+      <span data-test-id="answer">{{ answer }}</span>
+    </section>
+    <section>
+      <p>要素が存在するかどうかのテスト</p>
+      <button data-test-id="showButton" @click="onClickShowButton">表示する</button>
+      <p data-test-id="isShow" v-if="isShow">こんにちは</p>
+    </section>
   </div>
 </template>
--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,9 +1,10 @@
 import { describe, test, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import App from '@/App.vue';
+import { wrap } from 'module';
 
 describe(
-  'App.vueのテスト',
+  'App.vue 足し算のテスト',
   () => {
     test(
       '初期値の表示内容確認',
@@ -24,6 +25,32 @@ describe(
         await wrapper.get('[data-test-id="num2"]').setValue(9);
         const actual = wrapper.get('[data-test-id="answer"]').text();
         const expected = '19';
+        expect(actual).toBe(expected);
+      }
+    );
+  }
+);
+
+describe(
+  'App.vue 要素が存在するかどうかのテスト',
+  () => {
+    test(
+      '初期値の表示内容確認',
+      () => {
+        const wrapper = mount(App);
+        // 要素が存在するかどうか分からない場合は、get()ではなくfind()を使う
+        const actual = wrapper.find('[data-test-id="isShow"]').exists();
+        const expected = false;
+        expect(actual).toBe(expected);
+      }
+    );
+    test(
+      '表示するボタンを押した場合',
+      async () => {
+        const wrapper = mount(App);
+        await wrapper.get('[data-test-id="showButton"]').trigger('click');
+        const actual = wrapper.find('[data-test-id="isShow"]').exists();
+        const expected = true;
         expect(actual).toBe(expected);
       }
     );
